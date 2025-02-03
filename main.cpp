0e4d5dc10f55e1d384e09f59251c5f155b97fe80
Caitlynn115 <caitly@umich.edu>
2025-02-02 23:28:09 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+#include<string>
+using namespace std;
+class Profile{
+public:
+    void accessor()
+private:
+    string name, lastname;
+    int age;
+};
+{
+    cout<<"Name: "<<name;
+    cout<<"Last Name: "<<lastname;
+}
+     void mutator() {
+    cout<<"Age: "<<age;
+}
+class Twitter{
+public:
+private:
+    Profile TwProfile;
+    int NumFollowers;
+    void accessor();
+{
+cout<<"Name: "<<name;
+    cout<<"Last Name: "<<lastname;
+}
+    void mutator();
+{
+    cout<<"Age: "<<age;
+}
+};
+int main(){
+    Profile persona1;
+    personal.accessor("Laney""Lovett");
+    personal.mutator(23);
+    personal.getprofile();
+    Twitter Account1(persona1);
+}
+
+
+
+
+
+
+
+
+
+// TIP See CLion help at <a
+// href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>.
+//  Also, you can try interactive lessons for CLion by selecting
+//  'Help | Learn IDE Features' from the main menu.
