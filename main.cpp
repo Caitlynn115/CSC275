839169a29f7fab1ae6ecf131483ccafeeea806ee
Caitlynn115 <caitly@umich.edu>
2025-04-24 14:45:05 -0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,24 +1,81 @@
 #include <iostream>
+#include <stdexcept>
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+using namespace std;
+
+struct Node {
+    int val;
+    double u;
+    Node* next;
+};
+
+class LinkedList {
+public:
+    LinkedList() : head(nullptr), size(0) {
+
+    }
+
+    void addEl(int el);
+    void deleteHead();
+    int getSize() const;
+
+    friend ostream& operator << (ostream& os, LinkedList iList);
+
+private:
+    Node* head;
+    int size;
+};
+
+void LinkedList::addEl(int el) {
+    Node* n = new Node;
+    n->val = el;
+    n->u = 0.0;
+    n->next = head;
+    head = n;
+    ++ size;
+}
+
+void LinkedList::deleteHead() {
+    if (!head) {
+        throw runtime_error("List is empty. Cannot delete head.");
+    }
+    Node* temp = head;
+    head = head->next;
+    delete temp;
+    -- size;
+}
+
+int LinkedList::getSize() const {
+    return size;
+}
+
+ostream& operator<<(ostream& os, LinkedList iList) {
+    Node* curr = iList.head;
+    for (; curr != NULL; curr = curr->next) {
+        os << curr -> val << "->";
+    }
+    return os;
+}
+
 int main() {
-    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the
-    // <b>lang</b> variable name to see how CLion can help you rename it.
-    auto lang = "C++";
-    std::cout << "Hello and welcome to " << lang << "!\n";
+    LinkedList list1;
+    list1.addEl(10);
+    list1.addEl(20);
+    list1.addEl(30);
 
-    for (int i = 1; i <= 5; i++) {
-        // TIP Press <shortcut actionId="Debug"/> to start debugging your code.
-        // We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/>
-        // breakpoint for you, but you can always add more by pressing
-        // <shortcut actionId="ToggleLineBreakpoint"/>.
-        std::cout << "i = " << i << std::endl;
+    cout << "Initial List: " << list1;
+    cout << "Initial Size: " << list1.getSize();
+
+    try {
+        list1.deleteHead();
+        cout << "After Deleting Head: " << list1;
+        cout << "Size After Deletion: " << list1.getSize();
+    } catch (const exception & e) {
+        cout << "Error: " << e.what();
     }
 
     return 0;
 }
-
 // TIP See CLion help at <a
 // href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>.
 //  Also, you can try interactive lessons for CLion by selecting
